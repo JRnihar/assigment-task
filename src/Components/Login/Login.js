8e4cbc855337d5a3f0b8bb6f33b70292416a690d
JRnihar <niharmonirulislam@gmail.com>
2022-06-09 23:08:16 +0600

    final-commit2

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,7 +6,7 @@ import Loading from '../Loading/Loading'
 import { useSendPasswordResetEmail, useSignInWithEmailAndPassword} from 'react-firebase-hooks/auth';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import SocialLogin from './SocialLogin';
+
 
 const Login = () => {
     const [email, setEmail] = useState()
@@ -132,7 +132,7 @@ const Login = () => {
                                     </div>
                                 </div>
                             </div>
-                            <SocialLogin></SocialLogin>
+                            
                         </div>
                     </div>
                 </div>
